d67caac131c380813db7293e22b27c73138d1139
g-host-mailman <32482753+g-host-mailman@users.noreply.github.com>
2017-10-08 16:18:38 +1000

    update db name

--- wp-config-dev.php
+++ wp-config-dev.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'spark_cms');
+define('DB_NAME', 'spark_wordpress_cms');
 
 /** MySQL database username */
 define('DB_USER', 'root');
